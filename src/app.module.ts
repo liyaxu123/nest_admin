f684e67e0f0fe3f8b3933c61087a539ec6944323
liyaxu123 <2504862082@qq.com>
2024-11-22 22:45:28 +0800

    feat: 添加config配置并引入TypeORM连接MySQL数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigEnum } from './enums/config.enum';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 设置为全局模块
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return {
+          type: configService.get(ConfigEnum.DB_TYPE),
+          host: configService.get(ConfigEnum.MYSQL_HOST),
+          port: configService.get(ConfigEnum.MYSQL_PORT),
+          username: configService.get(ConfigEnum.MYSQL_USERNAME),
+          password: configService.get(ConfigEnum.MYSQL_PASSWORD),
+          database: configService.get(ConfigEnum.MYSQL_DATABASE),
+          entities: [],
+          synchronize: true,
+          logging: true,
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+          extra: {
+            authPlugins: 'sha256_password',
+          },
+        } as TypeOrmModuleOptions;
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/enums/config.enum.ts
@@ -0,0 +1,8 @@
+export enum ConfigEnum {
+  DB_TYPE = 'db_type',
+  MYSQL_HOST = 'mysql_server_host',
+  MYSQL_PORT = 'mysql_server_port',
+  MYSQL_DATABASE = 'mysql_server_database',
+  MYSQL_USERNAME = 'mysql_server_username',
+  MYSQL_PASSWORD = 'mysql_server_password',
+}
